3ab23355120abcc1d315dfd68028dacaed8662ce
Zhi Li <zli@babson.edu>
2018-02-12 11:11:08 -0500

    added inclass 06 .sql

---
+++ SQL/InClass-06.sql
@@ -0,0 +1,25 @@
+Use AdventureWorks2012;
+/*Using the HumanResource.Employee table, provide a count of the number of employees by job title.  The query should consider only current employees (the CurrentFlag must equal 1).  */
+
+
+
+
+
+
+/*Modify the query you created in Activity 1 so that the output shows only those job titles for which there is more than 1 employee.  */
+
+
+
+
+
+/*For each product, show its ProductID and Name (from the ProductionProduct table) and the location of its inventory (from the Product.Location table) and amount of inventory held at that location (from the Production.ProductInventory table).*/
+
+
+
+
+
+
+/*Find the product model IDs that have no product associated with them. */ 
+/*To do this, first do an outer join between the Production.Product table and the Production.ProductModel table in such a way that the ID from the ProductModel table always shows, even if there is no product associate with it. */
+/*Then, add a WHERE clause to specify that the ProductID IS NULL */
+

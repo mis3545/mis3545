782242bac2bc52715aabeab9ae6a02f85b12102d
Zhi Li <lzblack@users.noreply.github.com>
2018-02-14 10:43:10 -0500

    added session 07 sql

---
+++ SQL/Session_07_SQL_Exercises.sql
@@ -0,0 +1,32 @@
+USE AdventureWorks2012; /*Set current database*/
+
+
+/*1, Display the total amount collected from the orders for each order date. */
+
+
+/*2, Display the total amount collected from selling the products, FROM 700 to 800. Only list those products that have been sold more than 3000 units.*/
+
+
+
+/*3, Write a query to display the sales person BusinessEntityID, last name and first name of ALL the sales persons and the name of the territory to which they belong, even though they don't belong to any territory.*/
+
+
+
+/*4,  Write a query to display the names of the customers that have the 'Vista' credit card.*/
+/* Tables: Sales.CreditCard, Sales.PersonCreditCard, Person.Person*/
+
+
+
+/*5, Then show the number of customers for each type of credit cards.*/
+
+
+
+/*6, Write a query to display ALL the product names along with their corresponding subcategory names.*/
+/* Tables: Production.Product, Production.ProductSubcategory*/
+
+
+/*7. List ALL the product names that do not belong to any subcategory.*/
+
+
+
+/*8, Write a query to report the sales order ID of those orders where the total due is greater than the average of the total dues of all the orders. You need to use subquery. */

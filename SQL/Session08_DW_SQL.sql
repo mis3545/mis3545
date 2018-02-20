bcab454b88791b4aad929ff323d94bd8ecb30d21
Zhi Li <zli@babson.edu>
2018-02-20 09:46:47 -0500

    added DW sql

---
+++ SQL/Session08_DW_SQL.sql
@@ -0,0 +1,15 @@
+use AdventureWorksDW2012;
+
+/*Employees whose birthday is in February*/
+
+
+/*Who are the Sales Representatives whose birthday is in February?*/
+
+
+/*List all the sales processed by these Sales Representatives */
+
+
+/*who is a better sales representative that was born in February?*/
+ 
+ 
+/* Find a better way to compare these two salespersons?*/

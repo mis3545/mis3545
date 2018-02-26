b4d6c9373dfda740b20b2cc5a5a83de1f36de1e6
Zhi Li <zli@babson.edu>
2018-02-26 09:44:14 -0500

    added solutions

---
+++ SQL/SQL_quiz_1.sql
@@ -0,0 +1,62 @@
+USE AdventureWorksDW2012;
+/*a.	A customer has $1000 budget to spend on a single bike. Show the Product ID, Product Name, Price, and Color of each bike that she can purchase. These should be sorted by price in ascending order. */
+
+SELECT p.ProductID, p.Name AS Product_Name, p.ListPrice, p.Color
+FROM Production.Product AS p
+JOIN Production.ProductSubcategory AS ps ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+WHERE p.ListPrice <= 1000 AND ps.ProductCategoryID = 1
+ORDER BY p.ListPrice ASC;
+
+/*b.	Show First name and last name of employees whose job title is “Sales Representative”, ranking from oldest to youngest. */
+SELECT p.FirstName, p.LastName
+FROM HumanResources.Employee as e
+JOIN Person.Person AS p ON e.BusinessEntityID = p.BusinessEntityID
+WHERE e.JobTitle = 'Sales Representative'
+ORDER BY DATEDIFF(year, e.BirthDate, GETDATE());
+
+
+
+/*c.	Show the product ID, product name, and list price for each product where the list price is higher than the average list price for all products. */
+SELECT p.ProductID, p.Name, p.ListPrice
+FROM Production.Product AS p
+WHERE p.ListPrice > 
+	(SELECT AVG(ListPrice) 
+	FROM Production.Product);
+
+
+
+/*d.	Show the product category name, subcategory name, product name, and product ID for all products that belong to a category and subcategory. These should be sorted alphabetically by category name, within category by subcategory name, and within subcategory by product name. */
+SELECT pc.Name AS Product_Category_Name, ps.Name AS Product_Subcategory_Name, p.Name AS Product_Name, p.ProductID
+FROM Production.Product AS p
+JOIN Production.ProductSubcategory AS ps ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+JOIN Production.ProductCategory AS pc ON ps.ProductCategoryID = pc.ProductCategoryID
+ORDER BY Product_Category_Name, Product_Subcategory_Name, Product_Name;
+
+
+
+/*e.	Modify the query above (query d) so that it includes only those products that do not belong to a category or subcategory. */
+SELECT pc.Name AS Product_Category_Name, ps.Name AS Product_Subcategory_Name, p.Name AS Product_Name, p.ProductID
+FROM Production.Product AS p
+LEFT OUTER JOIN Production.ProductSubcategory AS ps ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+LEFT OUTER JOIN Production.ProductCategory AS pc ON ps.ProductCategoryID = pc.ProductCategoryID
+WHERE ps.ProductSubcategoryID IS NULL OR pc.ProductCategoryID IS NULL
+ORDER BY Product_Category_Name, Product_Subcategory_Name, Product_Name;
+
+/*f.	(bonus question) Find out the largest orders in each territory (in terms of total due) and their associated credit card numbers.*/
+
+SELECT st.name AS 'Location'
+,ss.TotalDue AS 'Top_Sale'
+,cc.CardNumber
+FROM Sales.CreditCard AS cc
+JOIN Sales.SalesOrderHeader AS ss 
+	ON ss.CreditCardID=cc.CreditCardID
+JOIN Sales.SalesTerritory AS st 
+	ON ss.TerritoryID=st.TerritoryID
+Join (
+	SELECT MAX(ss.TotalDue) AS 'Top_Sale', st.Name
+	FROM Sales.SalesOrderHeader AS ss 
+	JOIN Sales.SalesTerritory AS st ON ss.TerritoryID=st.TerritoryID
+	GROUP BY st.Name
+) as T 
+	On ss.TotalDue = T.Top_Sale AND st.Name = T.Name
+ORDER BY Location;
--- SQL/Session07_SQL_Exercises.sql
+++ SQL/Session07_SQL_Exercises.sql
@@ -2,31 +2,84 @@ USE AdventureWorks2012; /*Set current database*/
 
 
 /*1, Display the total amount collected from the orders for each order date. */
+SELECT OrderDate, SUM(TotalDue) as Daily_Amount,
+COUNT(SalesOrderID) as Number_of_Orders
+FROM Sales.SalesOrderHeader
+GROUP BY OrderDate
+ORDER BY SUM(TotalDue) DESC;
 
 
 /*2, Display the total amount collected from selling the products, FROM 700 to 800. Only list those products that have been sold more than 3000 units.*/
-
+SELECT ProductID, SUM(LineTotal) as Total_Amount,
+AVG(UnitPrice) as Avg_Unit_Price,
+MAX(UnitPrice) as Max_Unit_Price,
+Min(UnitPrice) as Min_Unit_Price,
+SUM(OrderQty) as 'Total Number of Units Sold'
+FROM Sales.SalesOrderDetail
+WHERE ProductID Between 700 and 800
+GROUP BY ProductID
+HAVING SUM(OrderQty)>3000
+ORDER BY SUM(OrderQty) DESC;
 
 
 /*3, Write a query to display the sales person BusinessEntityID, last name and first name of ALL the sales persons and the name of the territory to which they belong, even though they don't belong to any territory.*/
 
+SELECT SP.BusinessEntityID, 
+		P.FirstName, 
+		P.LastName, 
+		ST.Name as Territory
+FROM Sales.SalesPerson as SP
+FULL OUTER JOIN Sales.SalesTerritory as ST
+On SP.TerritoryID = ST.TerritoryID
+JOIN Person.Person as P
+ON SP.BusinessEntityID = P.BusinessEntityID;
 
 
 /*4,  Write a query to display the names of the customers that have the 'Vista' credit card.*/
 /* Tables: Sales.CreditCard, Sales.PersonCreditCard, Person.Person*/
+SELECT Person.BusinessEntityID, CardType,
+	LastName, FirstName
+FROM Sales.CreditCard
+JOIN Sales.PersonCreditCard
+ON Sales.CreditCard.CreditCardID = Sales.PersonCreditCard.CreditCardID
+JOIN Person.Person
+ON Sales.PersonCreditCard.BusinessEntityID=Person.BusinessEntityID
+WHERE CardType = 'Vista';
 
 
 
 /*5, Then show the number of customers for each type of credit cards.*/
-
+SELECT CardType, COUNT(BusinessEntityID) as Number_of_Customers
+FROM Sales.CreditCard
+JOIN Sales.PersonCreditCard
+ON Sales.CreditCard.CreditCardID = Sales.PersonCreditCard.CreditCardID
+GROUP BY CardType;
 
 
 /*6, Write a query to display ALL the product names along with their corresponding subcategory names.*/
 /* Tables: Production.Product, Production.ProductSubcategory*/
+SELECT pc.Name AS Product_Category_Name, ps.Name AS Product_Subcategory_Name, p.Name AS Product_Name, p.ProductID
+FROM Production.Product AS p
+JOIN Production.ProductSubcategory AS ps ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+JOIN Production.ProductCategory AS pc ON ps.ProductCategoryID = pc.ProductCategoryID
+ORDER BY Product_Category_Name, Product_Subcategory_Name, Product_Name;
+
 
 
 /*7. List ALL the product names that do not belong to any subcategory.*/
 
-
+SELECT ps.Name AS Product_Subcategory_Name, p.Name AS Product_Name, p.ProductID
+FROM Production.Product AS p
+LEFT OUTER JOIN Production.ProductSubcategory AS ps ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+WHERE ps.ProductSubcategoryID IS NULL 
+ORDER BY Product_Name;
 
 /*8, Write a query to report the sales order ID of those orders where the total due is greater than the average of the total dues of all the orders. You need to use subquery. */
+SELECT SalesOrderID, TotalDue
+FROM Sales.SalesOrderHeader
+WHERE TotalDue > 
+      (
+		SELECT AVG(TotalDue)
+		FROM Sales.SalesOrderHeader
+	  )
+ORDER BY TotalDue;
